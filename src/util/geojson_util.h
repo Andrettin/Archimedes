799a699a48686d10dceed2d34d2ee08f63b969ec
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-03 20:24:29 +0200

    Added geojson::create_geodata_map() utility function

--- src/util/geojson_util.h
+++ src/util/geojson_util.h
@@ -35,4 +35,50 @@ inline void process_feature_data(const QVariantMap &feature_data, const function
 	}
 }
 
+template <typename map_type, typename key_function_type, typename geopath_width_function_type>
+inline map_type create_geodata_map(const std::vector<QVariantList> &geojson_data_list, const key_function_type &key_function, const geopath_width_function_type &geopath_width_function)
+{
+	map_type geodata_map{};
+
+	geojson::process_features(geojson_data_list, [&](const QVariantMap &feature) {
+		const QVariantMap properties = feature.value("properties").toMap();
+
+		const map_type::key_type key = key_function(properties);
+
+		geojson::process_feature_data(feature, [&](const QVariantMap &feature_data) {
+			const QString type_str = feature_data.value("type").toString();
+
+			std::unique_ptr<QGeoShape> geoshape;
+
+			if (type_str == "LineString") {
+				const QGeoPath geopath = feature_data.value("data").value<QGeoPath>();
+				auto geopath_copy = std::make_unique<QGeoPath>(geopath);
+
+				if constexpr (!std::is_same_v<geopath_width_function_type, std::nullptr_t>) {
+					if (geopath_width_function != nullptr) {
+						const int geopath_width = geopath_width_function(key);
+						if (geopath_width != -1) {
+							geopath_copy->setWidth(geopath_width);
+						}
+					}
+				}
+
+				geoshape = std::move(geopath_copy);
+			} else if (type_str == "Polygon") {
+				const QGeoPolygon geopolygon = feature_data.value("data").value<QGeoPolygon>();
+				geoshape = std::make_unique<QGeoPolygon>(geopolygon);
+			} else if (type_str == "Point") {
+				const QGeoCircle geocircle = feature_data.value("data").value<QGeoCircle>();
+				geoshape = std::make_unique<QGeoCircle>(geocircle);
+			} else {
+				throw std::runtime_error("Invalid GeoJSON feature type string: \"" + type_str.toStdString() + "\".");
+			}
+
+			geodata_map[key].push_back(std::move(geoshape));
+		});
+	});
+
+	return geodata_map;
+}
+
 }
