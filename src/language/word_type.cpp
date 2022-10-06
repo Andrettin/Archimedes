9890938188a82e861be46b2e1876949d0184a717
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-06 19:51:21 +0200

    Revert "Update to the enum converter"
    This reverts commit 2d038aa91f838ef85f8627afa00107007936e380.

--- src/language/word_type.cpp
+++ src/language/word_type.cpp
@@ -7,21 +7,24 @@ namespace archimedes {
 template class enum_converter<word_type>;
 
 template <>
-const enum_converter<word_type> enum_converter<word_type>::instance = enum_converter(
-	"archimedes::word_type",
-	{
-		{ "none", word_type::none },
-		{ "noun", word_type::noun },
-		{ "verb", word_type::verb },
-		{ "adjective", word_type::adjective },
-		{ "pronoun", word_type::pronoun },
-		{ "adverb", word_type::adverb },
-		{ "conjunction", word_type::conjunction },
-		{ "adposition", word_type::adposition },
-		{ "article", word_type::article },
-		{ "numeral", word_type::numeral },
-		{ "affix", word_type::affix }
-	}
-);
+const std::string enum_converter<word_type>::property_class_identifier = "archimedes::word_type";
+
+template <>
+const std::map<std::string, word_type> enum_converter<word_type>::string_to_enum_map = {
+	{ "none", word_type::none },
+	{ "noun", word_type::noun },
+	{ "verb", word_type::verb },
+	{ "adjective", word_type::adjective },
+	{ "pronoun", word_type::pronoun },
+	{ "adverb", word_type::adverb },
+	{ "conjunction", word_type::conjunction },
+	{ "adposition", word_type::adposition },
+	{ "article", word_type::article },
+	{ "numeral", word_type::numeral },
+	{ "affix", word_type::affix }
+};
+
+template <>
+const bool enum_converter<word_type>::initialized = enum_converter::initialize();
 
 }
