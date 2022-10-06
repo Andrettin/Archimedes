2d038aa91f838ef85f8627afa00107007936e380
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-06 19:41:08 +0200

    Update to the enum converter

--- src/util/enum_converter.h
+++ src/util/enum_converter.h
@@ -12,21 +12,23 @@ template <typename enum_type>
 class enum_converter final : public enum_converter_base
 {
 private:
-	enum_converter()
+	explicit enum_converter(std::string &&property_class_identifier, std::map<std::string, enum_type> &&string_to_enum_map)
+		: property_class_identifier(std::move(property_class_identifier)),
+		string_to_enum_map(std::move(string_to_enum_map))
 	{
-		//this check is required for class_initialized variable and, correspondingly,
-		//the data_type::initialize_class() call to not to be initialized away
-		if (!enum_converter::initialized) {
-			throw std::runtime_error("Never reached.");
-		}
+		this->initialize_enum_to_string_map();
+
+		enum_converter_base::register_string_to_qvariant_conversion(this->property_class_identifier, [](const std::string &value) {
+			return QVariant::fromValue(enum_converter::to_enum(value));
+		});
 	}
 
 public:
 	static enum_type to_enum(const std::string &str)
 	{
-		const auto find_iterator = enum_converter::string_to_enum_map.find(str);
+		const auto find_iterator = enum_converter::instance.string_to_enum_map.find(str);
 
-		if (find_iterator != enum_converter::string_to_enum_map.end()) {
+		if (find_iterator != enum_converter::instance.string_to_enum_map.end()) {
 			return find_iterator->second;
 		}
 
@@ -35,9 +37,9 @@ public:
 
 	static const std::string &to_string(const enum_type enum_value)
 	{
-		const auto find_iterator = enum_converter::enum_to_string_map.find(enum_value);
+		const auto find_iterator = enum_converter::instance.enum_to_string_map.find(enum_value);
 
-		if (find_iterator != enum_converter::enum_to_string_map.end()) {
+		if (find_iterator != enum_converter::instance.enum_to_string_map.end()) {
 			return find_iterator->second;
 		}
 
@@ -45,29 +47,19 @@ public:
 	}
 
 private:
-	static bool initialize()
+	static const enum_converter instance;
+
+	void initialize_enum_to_string_map()
 	{
-		enum_converter::initialize_enum_to_string_map();
-
-		enum_converter_base::register_string_to_qvariant_conversion(enum_converter::property_class_identifier, [](const std::string &value) {
-			return QVariant::fromValue(enum_converter::to_enum(value));
-		});
-
-		return true;
-	}
-
-	static void initialize_enum_to_string_map()
-	{
-		for (const auto &[enum_str, enum_value] : enum_converter::string_to_enum_map) {
-			enum_converter::enum_to_string_map[enum_value] = enum_str;
+		for (const auto &[enum_str, enum_value] : this->string_to_enum_map) {
+			this->enum_to_string_map[enum_value] = enum_str;
 		}
 	}
 
 private:
-	static const std::string property_class_identifier;
-	static const std::map<std::string, enum_type> string_to_enum_map; //this should be defined where the enum converter is specialized
-	static inline std::map<enum_type, std::string> enum_to_string_map;
-	static const bool initialized;
+	const std::string property_class_identifier;
+	const std::map<std::string, enum_type> string_to_enum_map; //this should be defined where the enum converter is specialized
+	std::map<enum_type, std::string> enum_to_string_map;
 };
 
 }
