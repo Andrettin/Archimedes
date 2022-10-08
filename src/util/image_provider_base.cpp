06930e2ba56ba007a83399a2b1b4ae8becd2587e
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-10-08 10:59:17 +0200

    Added image provider base class

---
+++ src/util/image_provider_base.cpp
@@ -0,0 +1,41 @@
+#include "archimedes.h"
+
+#include "map/image_provider_base.h"
+
+#include "util/assert_util.h"
+
+namespace archimedes {
+
+QImage image_provider_base::requestImage(const QString &id, QSize *size, const QSize &requested_size)
+{
+	Q_UNUSED(requested_size);
+
+	const std::string id_str = id.toStdString();
+
+	const QImage &image = this->get_image(id_str);
+
+	assert_throw(!image.isNull());
+
+	if (size != nullptr) {
+		*size = image.size();
+	}
+
+	return image;
+}
+
+const QImage &image_provider_base::get_image(const std::string &id)
+{
+	auto find_iterator = this->image_map.find(id);
+
+	if (find_iterator != this->image_map.end()) {
+		return find_iterator->second;
+	}
+
+	this->load_image(id);
+
+	find_iterator = this->image_map.find(id);
+	assert_throw(find_iterator != this->image_map.end());
+	return find_iterator->second;
+}
+
+}
---
+++ src/util/image_provider_base.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#pragma warning(push, 0)
+#include <QQuickImageProvider>
+#pragma warning(pop)
+
+namespace archimedes {
+
+class image_provider_base : public QQuickImageProvider
+{
+public:
+	image_provider_base() : QQuickImageProvider(QQuickImageProvider::Image)
+	{
+	}
+
+	virtual QImage requestImage(const QString &id, QSize *size, const QSize &requested_size) override;
+
+	const QImage &get_image(const std::string &id);
+	virtual void load_image(const std::string &id) = 0;
+
+	void set_image(const std::string &id, QImage &&image)
+	{
+		this->image_map[id] = std::move(image);
+	}
+
+private:
+	std::map<std::string, QImage> image_map;
+};
+
+}
