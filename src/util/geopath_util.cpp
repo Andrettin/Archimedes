67d4ea39979dae1684853761436229f8da60ad28
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-12-24 18:42:00 +0100

    Added geopath::write_edges_to_image() utility function

--- src/util/geopath_util.cpp
+++ src/util/geopath_util.cpp
@@ -2,6 +2,7 @@
 
 #include "util/geopath_util.h"
 
+#include "map/direction.h"
 #include "map/map_projection.h"
 #include "util/geocoordinate.h"
 #include "util/georectangle.h"
@@ -32,6 +33,54 @@ void write_to_image(const QGeoPath &geopath, QImage &image, const QColor &color,
 		}
 
 		previous_pixel_pos = pixel_pos;
+	}
+}
+
+void write_edges_to_image(const QGeoPath &geopath, QImage &image, const georectangle &georectangle, const map_projection *map_projection, const int x_offset)
+{
+	const QRect image_rect = image.rect();
+
+	for (const QGeoCoordinate &qgeocoordinate : geopath.path()) {
+		const archimedes::geocoordinate geocoordinate(qgeocoordinate);
+		const QPoint pixel_pos = map_projection->geocoordinate_to_point(geocoordinate, georectangle, image.size(), x_offset);
+
+		if (!image_rect.contains(pixel_pos)) {
+			//only write to the image if the position is actually in it
+			continue;
+		}
+
+		uint8_t direction_flags = static_cast<uint8_t>(image.pixelColor(pixel_pos).blue());
+
+		uint8_t new_direction_flags = direction_flags;
+
+		const archimedes::geocoordinate tile_center_geocoordinate = map_projection->point_to_geocoordinate(pixel_pos, georectangle, image.size(), x_offset);
+
+		const QPoint reference_pos = map_projection->geocoordinate_to_point(geocoordinate, georectangle, image.size() * 100, x_offset * 100);
+		const QPoint tile_center_reference_pos = map_projection->geocoordinate_to_point(tile_center_geocoordinate, georectangle, image.size() * 100, x_offset * 100);
+		const int longitude_distance = std::abs(reference_pos.x() - tile_center_reference_pos.x());
+		const int latitude_distance = std::abs(reference_pos.y() - tile_center_reference_pos.y());
+
+		if (longitude_distance >= 50) {
+			if (geocoordinate.get_latitude() <= tile_center_geocoordinate.get_latitude()) {
+				new_direction_flags |= direction_flag::south;
+			} else {
+				new_direction_flags |= direction_flag::north;
+			}
+		}
+
+		if (latitude_distance >= 50) {
+			if (geocoordinate.get_longitude() >= tile_center_geocoordinate.get_longitude()) {
+				new_direction_flags |= direction_flag::east;
+			} else {
+				new_direction_flags |= direction_flag::west;
+			}
+		}
+
+		if (new_direction_flags == direction_flags) {
+			continue;
+		}
+
+		geoshape::write_pixel_to_image(pixel_pos, QColor(0, 0, new_direction_flags), image);
 	}
 }
 
--- src/util/geopath_util.h
+++ src/util/geopath_util.h
@@ -9,4 +9,6 @@ namespace archimedes::geopath {
 
 extern void write_to_image(const QGeoPath &geopath, QImage &image, const QColor &color, const georectangle &georectangle, const map_projection *map_projection, const int x_offset);
 
+extern void write_edges_to_image(const QGeoPath &geopath, QImage &image, const georectangle &georectangle, const map_projection *map_projection, const int x_offset);
+
 }
