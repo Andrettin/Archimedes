bddf5717ce4364f3bd2cea5c77dbcefd6f99c7ec
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-01-16 20:42:32 +0100

    Added name generator

---
+++ src/language/name_generator.cpp
@@ -0,0 +1,38 @@
+#include "archimedes.h"
+
+#include "name_generator.h"
+
+#include "util/gender.h"
+#include "util/vector_random_util.h"
+#include "util/vector_util.h"
+
+namespace archimedes {
+
+bool name_generator::is_name_valid(const std::string &name) const
+{
+	for (const name_variant &name_variant : this->names) {
+		if (get_name_variant_string(name_variant) == name) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void name_generator::add_names(const std::vector<name_variant> &names)
+{
+	vector::merge(this->names, names);
+}
+
+void name_generator::add_names(const std::vector<std::string> &names)
+{
+	vector::merge(this->names, names);
+}
+
+std::string name_generator::generate_name() const
+{
+	const name_variant &name_variant = vector::get_random(this->names);
+	return get_name_variant_string(name_variant);
+}
+
+}
---
+++ src/language/name_generator.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "language/name_variant.h"
+
+namespace archimedes {
+
+enum class gender;
+
+class name_generator final
+{
+public:
+	static constexpr size_t minimum_name_count = 10;
+
+	const std::vector<name_variant> &get_names() const
+	{
+		return this->names;
+	}
+
+	size_t get_name_count() const
+	{
+		return this->names.size();
+	}
+
+	bool is_name_valid(const std::string &name) const;
+
+	void add_name(const name_variant &name)
+	{
+		this->names.push_back(name);
+	}
+
+	void add_names(const std::vector<name_variant> &names);
+	void add_names(const std::vector<std::string> &names);
+
+	std::string generate_name() const;
+
+private:
+	std::vector<name_variant> names; //name list for generation
+};
+
+}
