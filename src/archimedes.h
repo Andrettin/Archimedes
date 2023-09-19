39f2797dbf156a7271dabd043483101d79127c81
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-19 17:52:37 +0200

    Updated to Qt6

--- src/archimedes.h
+++ src/archimedes.h
@@ -2,5 +2,20 @@
 
 #include "pch.h"
 
+//forward declarations
+
 namespace archimedes {
+	class calendar;
+	class language;
+	class language_family;
+	class map_projection;
+	class timeline;
+	class word;
 }
+
+Q_DECLARE_OPAQUE_POINTER(archimedes::calendar*)
+Q_DECLARE_OPAQUE_POINTER(archimedes::language*)
+Q_DECLARE_OPAQUE_POINTER(archimedes::language_family*)
+Q_DECLARE_OPAQUE_POINTER(archimedes::map_projection*)
+Q_DECLARE_OPAQUE_POINTER(archimedes::timeline*)
+Q_DECLARE_OPAQUE_POINTER(archimedes::word*)
