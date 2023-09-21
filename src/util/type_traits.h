18923f3827a8b5a59bd7d35f179403dd83c11bd5
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-21 20:10:14 +0200

    Minor update for the type traits

--- src/util/type_traits.h
+++ src/util/type_traits.h
@@ -2,13 +2,13 @@
 
 namespace archimedes {
 
-template<typename T, template<typename...> class T2>
+template<typename T, template<typename...> typename T2>
 struct is_specialization_of : std::false_type {};
 
-template<template<typename...> class T, typename ...A>
+template<template<typename...> typename T, typename ...A>
 struct is_specialization_of<T<A...>, T> : std::true_type {};
 
-template<typename T, template<typename...> class T2>
+template<typename T, template<typename...> typename T2>
 inline constexpr bool is_specialization_of_v = is_specialization_of<T, T2>::value;
 
 }
