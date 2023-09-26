fefc95f7032a4233485c9f9397ca33f0e82e51c5
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-26 18:14:14 +0200

    Added support for writing logs from standard output to a file

--- src/util/log_output_handler.h
+++ src/util/log_output_handler.h
@@ -4,31 +4,47 @@ namespace archimedes {
 
 class log_output_handler final
 {
-public:
-	explicit log_output_handler(const std::filesystem::path &error_log_path)
-		: error_log_path(error_log_path)
+private:
+	static void init_log_file(const std::filesystem::path &filepath, std::ostream &ostream, std::ofstream &filestream, std::streambuf *buf)
 	{
-		if (std::filesystem::exists(this->error_log_path) && std::filesystem::file_size(this->error_log_path) > 1000000) {
-			std::filesystem::remove(this->error_log_path);
+		if (std::filesystem::exists(filepath) && std::filesystem::file_size(filepath) > 1000000) {
+			std::filesystem::remove(filepath);
 		}
 
-		this->error_stream = std::ofstream(this->error_log_path);
-		this->cerr_buf = std::cerr.rdbuf();
-		std::cerr.rdbuf(this->error_stream.rdbuf());
+		filestream = std::ofstream(filepath);
+		buf = ostream.rdbuf();
+		ostream.rdbuf(filestream.rdbuf());
+	}
+
+	static void close_log_file(const std::filesystem::path &filepath, std::ostream &ostream, std::ofstream &filestream, std::streambuf *buf)
+	{
+		ostream.clear();
+		ostream.rdbuf(buf);
+		filestream.close();
+
+		if (std::filesystem::exists(filepath) && std::filesystem::file_size(filepath) == 0) {
+			std::filesystem::remove(filepath);
+		}
+	}
+
+public:
+	explicit log_output_handler(const std::filesystem::path &output_log_path, const std::filesystem::path &error_log_path)
+		: output_log_path(output_log_path), error_log_path(error_log_path)
+	{
+		this->init_log_file(output_log_path, std::cout, this->output_stream, this->cout_buf);
+		this->init_log_file(error_log_path, std::cerr, this->error_stream, this->cerr_buf);
 	}
 
 	~log_output_handler()
 	{
-		std::cerr.clear();
-		std::cerr.rdbuf(this->cerr_buf);
-		this->error_stream.close();
-
-		if (std::filesystem::exists(this->error_log_path) && std::filesystem::file_size(this->error_log_path) == 0) {
-			std::filesystem::remove(this->error_log_path);
-		}
+		this->close_log_file(output_log_path, std::cout, this->output_stream, this->cout_buf);
+		this->close_log_file(error_log_path, std::cerr, this->error_stream, this->cerr_buf);
 	}
 
 private:
+	const std::filesystem::path output_log_path;
+	std::ofstream output_stream;
+	std::streambuf *cout_buf = nullptr;
 	const std::filesystem::path error_log_path;
 	std::ofstream error_stream;
 	std::streambuf *cerr_buf = nullptr;
