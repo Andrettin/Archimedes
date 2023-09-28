bafdebe970c137baf4b8d42e3675705b2576b931
Andrettin <6322423+Andrettin@users.noreply.github.com>
2023-09-28 19:34:06 +0200

    Build QCoro

--- src/pch.h
+++ src/pch.h
@@ -75,6 +75,10 @@
 #include <QTranslator>
 #include <QVariant>
 #include <QVariantList>
+
+#include <qcoro/core/qcorofuture.h>
+#include <qcoro/qcorotask.h>
+#include <qcoro/qml/qcoroqmltask.h>
 #pragma warning(pop)
 
 #include "util/point_operators.h" //to ensure the / operator with an int as RHS is used instead of the Qt one with qreal (which uses rounding)
