cd459e2e0caf3cf781cc7d1fbc01b5d25d3f2db7
Andrettin <6322423+Andrettin@users.noreply.github.com>
2025-09-08 12:47:06 +0200

    Fixed dice::to_string() function

--- src/util/dice.cpp
+++ src/util/dice.cpp
@@ -9,9 +9,9 @@ namespace archimedes {
 std::string dice::to_string() const
 {
 	if (this->get_modifier() != 0) {
-		return std::format("{}d{}", this->get_count(), this->get_sides());
-	} else {
 		return std::format("{}d{}{}", this->get_count(), this->get_sides(), number::to_signed_string(this->get_modifier()));
+	} else {
+		return std::format("{}d{}", this->get_count(), this->get_sides());
 	}
 }
 
